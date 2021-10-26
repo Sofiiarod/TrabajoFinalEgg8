993128df36c8f1e51c2d88873a9bafe42b731c28
Sofia Rodriguez <sofiarodriguez95@hotmail.com>
2021-10-26 20:36:54 -0300

    Primer commit

---
+++ Scire/src/test/java/com/scire/ScireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
