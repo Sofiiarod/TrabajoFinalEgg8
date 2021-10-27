c96442a5d1f9f20bb37d6d261a89520aee5cce48
Sofia Rodriguez <sofiarodriguez95@hotmail.com>
2021-10-27 20:52:43 -0300

    modificaciones en UsuarioSerivicio, creo static y templates

--- Scire/src/main/java/com/scire/repositorios/UsuarioRepositorio.java
+++ Scire/src/main/java/com/scire/repositorios/UsuarioRepositorio.java
@@ -1,9 +1,15 @@
 package com.scire.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.scire.entidades.Usuario;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario,String> {
 
+	@Query("SELECT a from Usuario a WHERE a.email LIKE :email")
+	public Usuario encontrarPorEmail(@Param("email") String email);
+	
+	
 }
