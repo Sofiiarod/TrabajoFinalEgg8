1c67bcf4539a8941b67192f26d8e6873698df974
ignacio <ignacio.sepulveda069@gmail.com>
2021-10-27 21:53:52 -0300

    Modificacion en repositorios y servicios de categoria y creador

--- Scire/src/main/java/com/scire/repositorios/CategoriaRepositorio.java
+++ Scire/src/main/java/com/scire/repositorios/CategoriaRepositorio.java
@@ -1,9 +1,15 @@
 package com.scire.repositorios;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.scire.entidades.Categoria;
 
+@Repository
 public interface CategoriaRepositorio extends JpaRepository<Categoria,String> {
-
+	@Query ("SELECT c FROM Categoria c WHERE c.nombre = :nombre")
+	public Categoria buscarPorNombre (@Param("nombre") String nombre);
 }
