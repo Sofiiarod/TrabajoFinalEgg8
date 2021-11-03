46eb4770d5151840135999d695043b3a03d6f42e
Ivan Sotelo <soteloivanmanuel26@gmail.com>
2021-11-03 01:34:14 -0300

    se agrega modelo al main controller y se integra al index.html

--- Scire/src/main/java/com/scire/controladores/MainControlador.java
+++ Scire/src/main/java/com/scire/controladores/MainControlador.java
@@ -10,16 +10,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.scire.entidades.Curso;
+import com.scire.servicios.CursoServicio;
+
 
 
 @Controller
 @RequestMapping("/")
 public class MainControlador {
 	
-	
+	@Autowired
+	CursoServicio cursoServicio;
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(ModelMap modelo) {
+		List<Curso> cursos = cursoServicio.listarTodos();
+		Curso curso1 = cursos.get(0);
+		Curso curso2 = cursos.get(1);
+		Curso curso3 = cursos.get(2);
+		Curso curso4 = cursos.get(3);
+		modelo.addAttribute("curso1", curso1);
+		modelo.addAttribute("curso2", curso2);
+		modelo.addAttribute("curso3", curso3);
+		modelo.addAttribute("curso4", curso4);
 	return "../template/index.html";
 	
 	}
