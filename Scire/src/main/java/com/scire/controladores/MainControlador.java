015c89b719bf5fda40c7788af6d03a1887a7e407
Ivan Sotelo <soteloivanmanuel26@gmail.com>
2021-11-04 20:34:17 -0300

    Se agregan elementos a la vista y dos metodos a curso Servicio

--- Scire/src/main/java/com/scire/controladores/MainControlador.java
+++ Scire/src/main/java/com/scire/controladores/MainControlador.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.scire.entidades.Curso;
+import com.scire.errores.ErrorException;
 import com.scire.servicios.CursoServicio;
 
 
@@ -22,17 +23,38 @@ public class MainControlador {
 	@Autowired
 	CursoServicio cursoServicio;
 	
+	/**
+	 * 
+	 * @param modelo
+	 * @return String de index con plantillas 
+	 * de las imagenes de los videos cargados en base de datos
+	 */
 	@GetMapping("/")
 	public String index(ModelMap modelo) {
 		List<Curso> cursos = cursoServicio.listarTodos();
-		Curso curso1 = cursos.get(0);
-		Curso curso2 = cursos.get(1);
-		Curso curso3 = cursos.get(2);
-		Curso curso4 = cursos.get(3);
-		modelo.addAttribute("curso1", curso1);
-		modelo.addAttribute("curso2", curso2);
-		modelo.addAttribute("curso3", curso3);
-		modelo.addAttribute("curso4", curso4);
+		
+		try {
+			Curso curso1 = cursos.get(0);
+			Curso curso2 = cursos.get(1);
+			Curso curso3 = cursos.get(2);
+			Curso curso4 = cursos.get(3);
+			
+			modelo.addAttribute("curso1", curso1);
+			modelo.addAttribute("curso2", curso2);
+			modelo.addAttribute("curso3", curso3);
+			modelo.addAttribute("curso4", curso4);
+			
+			modelo.addAttribute("plantilla1", cursoServicio.urlImagen(curso1.getId()));
+			modelo.addAttribute("plantilla2", cursoServicio.urlImagen(curso2.getId()));
+			modelo.addAttribute("plantilla3", cursoServicio.urlImagen(curso3.getId()));
+			modelo.addAttribute("plantilla4", cursoServicio.urlImagen(curso4.getId()));
+			
+			
+		}catch(ErrorException e) {
+			e.getMessage();
+		}
+
+		
 	return "../template/index.html";
 	
 	}
