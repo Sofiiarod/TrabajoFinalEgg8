48add4a3a8c1444d0de0f573dd17448d8334870f
ignacio <ignacio.sepulveda069@gmail.com>
2021-11-17 22:43:42 -0300

    agrego admin a header

--- Scire/src/main/java/com/scire/controladores/UsuarioControlador.java
+++ Scire/src/main/java/com/scire/controladores/UsuarioControlador.java
@@ -34,21 +34,22 @@ public class UsuarioControlador {
 	public String registrar(ModelMap model, MultipartFile archivo, @RequestParam String nombre,
 			@RequestParam String apellido, @RequestParam String email, @RequestParam String clave,
 			@RequestParam String clave2) throws ErrorException {
-		try {
-			if (archivo == null) {
+
+			try {
+				if (archivo == null) {
 				usuarioServicio.guardar(null, nombre, apellido, email, clave, clave2);
+				}
+				usuarioServicio.guardar(archivo, nombre, apellido, email, clave, clave2);
+				model.put("exito", "Se ha registrado con exito");
+			} catch (ErrorException e) {
+
+				model.put("error", e.getMessage());
+
+				return "registrarse.html";
+
 			}
-			usuarioServicio.guardar(archivo, nombre, apellido, email, clave, clave2);
-			model.put("exito", "Se ha registrado con exito");
-		} catch (ErrorException e) {
-			model.put("error", e.getMessage());
 
-			return "registrarse.html";
-
-		}
-
-		model.put("exito", "Se ha registrado con exito");
-		return "redirect:/login";
+			return "redirect:/login";
 
 	}
 
@@ -65,8 +66,11 @@ public class UsuarioControlador {
 		try {
 
 			Usuario usuario = usuarioServicio.buscarPorId(id);
+
 			model.addAttribute("perfil", usuario);
+
 			return "/perfil/index-perfil";
+
 
 		} catch (ErrorException e) {
 
@@ -87,106 +91,164 @@ public class UsuarioControlador {
 															// registrado
 	@GetMapping("/editar-perfil")
 	public String editarPerfil(HttpSession session, @RequestParam String id, ModelMap model) {
-		Usuario logueado = (Usuario) session.getAttribute("usuariosession"); // aca va a obtener y usar un usuario
-																				// logueado usa logueado como variable
-		if (logueado == null || !logueado.getId().equals(id)) {// si logueado es null significa que en esa session no
-																// hay ningun usuario
-			return "index.html"; // || y si el usuario logueado no es igual al usuario que quiere
-									// modificar lo mando a la csm
-
+		Usuario logueado = (Usuario) session.getAttribute("usuariosession"); // aca va a obtener y usar un usuario logueado usa logueado como variable
+		if (logueado == null || !logueado.getId().equals(id)) {// si logueado es null significa que en esa session no hay ningun usuario
+			
+			return "index.html"; // || y si el usuario logueado no es igual al usuario que quiere modificar lo mando a la csm
 		}
 		try {
+
 			Usuario usuario = usuarioServicio.buscarPorId(id);
+
 			model.addAttribute("perfil", usuario);
+
 			return "/perfil/editar-perfil.html";
+
 		} catch (ErrorException e) {
+
 			System.out.println("Error 79: " + e.getMessage());
 
 		}
+
 		return "/perfil/editar-perfil.html";
+
 	}
 
-	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN' )") // El Usuario puede editar el perfil si solo si esta
-															// registrado
+
+	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN' )") // El Usuario puede editar el perfil si solo si está registrado
 	@PostMapping("/actualizar-perfil")
 	public String actualizar(ModelMap model, HttpSession session, @RequestParam String id, @RequestParam String nombre,
 			@RequestParam String apellido, String email) {
-		String idRedirect = "";
-		Usuario logueado = (Usuario) session.getAttribute("usuariosession");
-		if (logueado == null || !logueado.getId().equals(id)) {
+
+			String idRedirect = "";	
+
+			Usuario logueado = (Usuario) session.getAttribute("usuariosession");
+
+			if (logueado == null || !logueado.getId().equals(id)) {
+
 			return "index.html";
-		}
-		System.out.println("Error");
-		try {
-			Usuario usuario = usuarioServicio.buscarPorId(id);
-			usuarioServicio.modificarDatos(id, nombre, apellido, email);
-			session.setAttribute("usuariosession", usuario); // es para usar el usuario logueado en thymeleaf
-			idRedirect = "?id=".concat(usuario.getId());
-			return "redirect:/usuario/editar-perfil".concat(idRedirect);
-		} catch (ErrorException e) {
-			// model.addAttribute("error", e.getMessage());
-			System.out.println("Error: 103" + e.getMessage());
-			return "/perfil/editar-perfil.html";
-		}
+
+			}
+
+			try {
+
+				Usuario usuario = usuarioServicio.buscarPorId(id);
+
+				usuarioServicio.modificarDatos(id, nombre, apellido, email);
+
+				session.setAttribute("usuariosession", usuario); // es para usar el usuario logueado en thymeleaf
+
+				idRedirect = "?id=".concat(usuario.getId());
+
+				return "redirect:/usuario/editar-perfil".concat(idRedirect);
+
+			} catch (ErrorException e) {
+
+				model.addAttribute("error", e.getMessage());
+
+				System.out.println("Error: 103" + e.getMessage());
+
+				return "/perfil/editar-perfil.html";
+			
+			}
+
 
 	}
+
 
 	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN' )")
 	@GetMapping("/editar-foto")
 	public String editarFoto(HttpSession session, @RequestParam String id, ModelMap model, MultipartFile archivo) {
+
 		Usuario logueado = (Usuario) session.getAttribute("usuariosession");
+
 		if (logueado == null || !logueado.getId().equals(id)) {
+
 			return "redirect:/";
+
 		}
+
 		try {
+
 			Usuario usuario = usuarioServicio.buscarPorId(id);
+
 			model.addAttribute("perfil", usuario);
+
 			return "/perfil/editar-foto.html";
+
 		} catch (ErrorException e) {
+
 			System.out.println("Error 121: " + e.getMessage());
 		}
+
 		return "/perfil/editar-foto.html";
+
 	}
+
 
 	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN' )")
 	@PostMapping("/guardar-foto")
 	public String guardarFoto(ModelMap model, HttpSession session, @RequestParam String id, MultipartFile archivo) {
+
 		String idRedirect = "";
+
 		Usuario logueado = (Usuario) session.getAttribute("usuariosession");
+
 		if (logueado == null || !logueado.getId().equals(id)) {
+
 			return "redirect:/";
+
 		}
-		System.out.println("Error");
+
 		try {
+
 			Usuario usuario = usuarioServicio.buscarPorId(id);
-			System.out.println("idUSuario: " + usuario.getId());
-			// System.out.println("idFoto: " + usuario.getFoto().getId());
+
 			usuarioServicio.modificarFoto(id, archivo);
+
 			session.setAttribute("usuariosession", usuario);
+
 			idRedirect = "?id=".concat(usuario.getId());
+
 			return "redirect:/usuario/editar-foto".concat(idRedirect);
+
 		} catch (ErrorException e) {
+
 			System.out.println("Error: 138" + e.getMessage());
+
 			return "/perfil/editar-foto.html";
+
 		}
+
 	}
 
 	@GetMapping("/recuperar")
 	public String recuperacion() {
+
 		return "recuperar-contra.html";
+
 	}
 
 	@PostMapping("/recuperar")
 	public String recuperar(ModelMap model, @RequestParam String email) throws ErrorException {
+
 		try {
+
 			if (checkearEmail(email)) {
+
 				usuarioServicio.recuperarContrasenia(email);
+
 			}
 		} catch (ErrorException e) {
+
 			model.addAttribute("error", e.getMessage());
+
 			return "recuperar-contra.html";
+
 		}
+
 		model.put("exito", "Se ha enviado tu nueva contraseña a tu mail. Luego podrás cambiarla en tu perfil.");
+		
 		return "login.html";
 
 	}
